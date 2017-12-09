aeacb8884390ec57998b6a45ce144c447d1f02fc
jasmine <jasliu2012@gmail.com>
2017-12-09 14:58:08 -0600

    Deleted App content, replaced with Toolbar and NoteContainer components

--- src/App.js
+++ src/App.js
@@ -5,31 +5,8 @@ class App extends Component {
   render() {
     return (
       <div id="app">
-        <div className="toolbar">
-          <button className="toolbar-button">New</button>
-          <button className="toolbar-button">Delete</button>
-          <input className="toolbar-search" type="text" placeholder="Search..." />
-        </div>
-        <div className="note-container">
-          <div className="note-selectors">
-            <div className="note-selector active">
-              <p className="note-selector-title">First note...</p>
-              <p className="note-selector-timestamp">Timestamp here...</p>
-            </div>
-            <div className="note-selector active">
-              <p className="note-selector-title">Second note...</p>
-              <p className="note-selector-timestamp">Timestamp here...</p>
-            </div>
-            <div className="note-selector active">
-              <p className="note-selector-title">Third note...</p>
-              <p className="note-selector-timestamp">Timestamp here...</p>
-            </div>
-          </div>
-          <div className="note-editor">
-            <p className="note-editor-info">Timestamp here...</p>
-            <textarea className="note-editor-input"></textarea>
-          </div>
-        </div>
+        <Toobar />
+        <NoteContainer />
       </div>
     );
   }
