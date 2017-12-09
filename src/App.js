69ff193320b4b06961c7dd880da3dc84f55e4d9f
jasmine <jasliu2012@gmail.com>
2017-12-09 15:23:18 -0600

    Defined custom constructor and state for notes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
-import Toolbar from './components/Toolbar';
-import NoteContainer from './components/NoteContainer';
+import Toolbar from './components/Toolbar'
+import NoteContainer from './components/NoteContainer'
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      notes: [
+        {id: 1, body: "This is a first test", timestamp: Date.now()},
+        {id: 2, body: "This is a second test", timestamp: Date.now()},
+        {id: 3, body: "This is a third test", timestamp: Date.now()}
+      ]
+    }
+  }
+
   render() {
     return (
       <div id="app">
         <Toolbar />
-        <NoteContainer />
-      </div>
+        <NoteContainer notes={this.state.notes} />
+      </div>      
     );
   }
 }
