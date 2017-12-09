bf8932ddbadcca3f9f2df71ef6c253670efb22de
jasmine <jasliu2012@gmail.com>
2017-12-09 15:02:32 -0600

    imported Toolbar and Notecontainer components from components folder into App

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import Toolbar from './components/Toolbar';
+import NoteContainer from './components/NoteContainer';
 import './App.css';
 
 class App extends Component {
