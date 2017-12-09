f3bad5b9c8b30876e5fe5f72593afb912190f8fd
jasmine <jasliu2012@gmail.com>
2017-12-09 15:41:52 -0600

    Create array of NoteSelectors to be stored in variable noteSelectors and rendered in JSX. Passed down props to NoteSelector

--- src/components/NoteSelectors.js
+++ src/components/NoteSelectors.js
@@ -1,1 +1,22 @@
-import 
+import React, { Component } from 'react';
+import NoteSelector from './NoteSelector';
+
+class NoteSelectors extends Component {
+  render() {
+    const noteSelectors = this.props.notes.map(note => 
+      <NoteSelector
+        key={note.id}
+        body={note.body}
+        timestamp={note.timestamp}
+      />
+    );
+
+    return (
+      <div className="note-selectors">
+        {noteSelectors}
+      </div>
+    );
+  }
+}
+
+export default NoteSelectors;
